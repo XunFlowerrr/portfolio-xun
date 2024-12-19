3df4ee58fa08ae2d6a5cd24c5c0b4b499e07c390
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-12-20 02:24:42 +0700

    Add assetPrefix for production environment in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/portfolio-xun" : "",
+  assetPrefix: isProd ? "/portfolio-xun/" : "",
   output: "export",
   distDir: "dist",
   images: {
