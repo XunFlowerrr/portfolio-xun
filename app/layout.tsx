a274d126495d43576bbf0ec4c04147afe3437506
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-12-20 02:51:12 +0700

    Update favicon path to include 'portfolio-xun' prefix in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/github_profile.jpg" sizes="any" />
+        <link rel="icon" href="/portfolio-xun/github_profile.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
